753ff8ec8ccf2d8d25f336e207d75af2defa663f
sania bhutiani <saniabhutiani@gmail.com>
2026-02-22 17:15:53 +0530

    Updated service images

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
-import s1 from "../assets/skills/service1.jpg";
-import s2 from "../assets/skills/service2.jpg";
-import s3 from "../assets/skills/wall1.jpg";
-import s4 from "../assets/skills/wall2.jpg";
-import s5 from "../assets/skills/bedroom1.jpg";
-import s6 from "../assets/skills/modularkitchen1.jpg";
-import s7 from "../assets/skills/office1.jpg";
-import s8 from "../assets/skills/bedroom2.jpg";
-import s9 from "../assets/skills/modularkitchen2.jpg";
+import s1 from "../assets/skills/wallpaper1.jpg";
+import s2 from "../assets/skills/flooring.jpg";
+import s3 from "../assets/skills/falseceiling.jpg";
+import s4 from "../assets/skills/blind.jpg";
+import s5 from "../assets/skills/artificalgarden.jpg";
+import s6 from "../assets/skills/singlehouse.jpg";
+import s7 from "../assets/skills/allivation.jpg";
+import s8 from "../assets/skills/bedroominterior.jpg";
+import s9 from "../assets/skills/stretchceiling.jpg";
 
 export default function Services() {
 
@@ -19,11 +19,11 @@ export default function Services() {
     { img: s2, title: "Flooring", desc: "Luxury wooden & vinyl flooring." },
     { img: s3, title: "Ceiling Design", desc: "Modern ceiling concepts." },
     { img: s4, title: "Blinds", desc: "Stylish window blinds solutions." },
-    { img: s5, title: "Artificial Garden", desc: "Beautiful indoor greenery setup." },
-    { img: s6, title: "Wood Paneling", desc: "Elegant wooden wall panels." },
-    { img: s7, title: "PVC Panels", desc: "Durable & aesthetic PVC work." },
-    { img: s8, title: "Star Ceiling", desc: "Creative lighting ceiling design." },
-    { img: s9, title: "Interior Styling", desc: "Complete luxury interior setup." },
+    { img: s5, title: "Artifical Garden", desc: "Beautiful indoor greenery setup." },
+    { img: s6, title: "Shingles", desc: "Elegant wooden wall panels." },
+    { img: s7, title: "Allivation", desc: "Durable & aesthetic PVC work." },
+    { img: s8, title: "Bedroom Interior", desc: "Creative lighting ceiling design." },
+    { img: s9, title: "Stretch Ceiling", desc: "Complete luxury interior setup." },
   ];
 
   return (
